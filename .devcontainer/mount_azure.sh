490557d1c20b5c42d0b8cd2a572d0f3e773f9bb0
WV-Habitat <willian.vieira@habitat-nature.com>
2024-11-25 16:21:39 -0500

    add dev containers

---
+++ .devcontainer/mount_azure.sh
@@ -0,0 +1,16 @@
+RESOURCE_GROUP_NAME="HabitatNature-Prod"
+STORAGE_ACCOUNT_NAME="hnatprojets"
+FILE_SHARE_NAME="projet-et-database"
+MNT_PATH="$(pwd)/P"
+
+sudo mkdir -p $MNT_PATH
+
+az login
+
+# This command assumes you have logged in with az login
+HTTP_ENDPOINT=$(az storage account show --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --query "primaryEndpoints.file" --output tsv | tr -d '"')
+SMB_PATH=$(echo $HTTP_ENDPOINT | cut -c7-${#HTTP_ENDPOINT})$FILE_SHARE_NAME
+
+STORAGE_ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv | tr -d '"')
+
+sudo mount -t cifs $SMB_PATH $MNT_PATH -o username=$STORAGE_ACCOUNT_NAME,password=$STORAGE_ACCOUNT_KEY,serverino,nosharesock,actimeo=30,mfsymlinks
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM ubuntu:22.04
 
+# Add Rprofile for vscode-r extension and httpgd graphics
+COPY .devcontainer/.Rprofile /root/.Rprofile
+
 # Install Azure file storage tools and azure cli
 RUN apt-get update
 RUN apt-get -yq install sudo curl iptables wget tcpdump dnsutils netcat mount cifs-utils samba smbclient
 RUN curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
-# Add script to mount Azure file storage inside the Docker
-COPY mount_azure.sh .
